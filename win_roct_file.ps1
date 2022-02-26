4f079aaaba073a93082e1b1420bf83e7d33ea31b
Arif <aroute@users.noreply.github.com>
2022-02-25 20:21:52 -0600

    Added powershell script for file.

---
+++ win_roct_file.ps1
@@ -0,0 +1,40 @@
+oc new-project roct
+oc create serviceaccount roct
+oc adm policy add-scc-to-user privileged -n roct -z roct
+@"
+apiVersion: apps/v1
+kind: StatefulSet
+metadata:
+  name: roct
+spec:
+  selector:
+    matchLabels:
+      app: roct
+  replicas: 1
+  template:
+    metadata:
+      labels:
+        app: roct
+    spec:
+      serviceAccount: roct
+      containers:
+      - name: roct
+        securityContext:
+          privileged: true
+        image: quay.io/aroute/roct
+        imagePullPolicy: Always
+        volumeMounts:
+        - mountPath: /home/demo
+          name: roct-storage
+  volumeClaimTemplates:
+  - metadata:
+      name: roct-storage
+    spec:
+      accessModes:
+      - ReadWriteMany
+      resources:
+        requests:
+          storage: 20Gi
+      storageClassName: ibmc-file-gold-gid
+"@ > roct.yaml
+oc create -f roct.yaml
